05139b3ca85ca543d2775f4c739ac0fe2aa1109d
cferdinandi <chris@gomakethings.com>
2017-10-29 20:21:44 -0400

    v1.4.1 Fixed country check

--- includes/checkout.php
+++ includes/checkout.php
@@ -27,7 +27,7 @@
 		$discount_country = get_post_meta( $discount[0]->ID, 'pricing_parity_country', true );
 
 		// If the code is valid for the visitor's country, carry on
-		if ($visitor_country === $discount_country) return $return;
+		if ($visitor_country['country_code'] && $visitor_country['country_code'] === $discount_country) return $return;
 
 		// Otherwise, return an error
 		edd_set_error( 'edd-discount-error', _x( 'Sorry, this discount code is only for visitors from another country.', 'error for when a discount is invalid based on visitor location' , 'easy-digital-downloads' ) );
