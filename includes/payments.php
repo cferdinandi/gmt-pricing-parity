ed04509e67196be8397e5f41037770020a6c4350
cferdinandi <chris@gomakethings.com>
2022-10-24 13:39:54 -0400

    Updated parity  not displaying in UI

--- includes/payments.php
+++ includes/payments.php
@@ -6,19 +6,18 @@
 
 
 	function gmt_pricing_parity_discount_details($payment_id) {
-		$payment = edd_get_payment($payment_id);
-		$payment_meta = $payment->get_meta();
+		$payment_meta = edd_get_payment_meta($payment_id);
 		$discount = $payment_meta['pricing_parity'];
 		?>
-			<div class="edd-order-pricing-parity edd-admin-box-inside">
-				<p>
-					<span class="label"><?php _e( 'Pricing Parity:', 'pricing_parity' ); ?></span>&nbsp;
+			<div class="edd-order-pricing-parity edd-admin-box-inside edd-admin-box-inside--row">
+				<span class="label"><?php _e( 'Pricing Parity:', 'pricing_parity' ); ?></span>
+				<span class="value">
 					<?php if (empty($discount)) : ?>
 					<?php _e( 'none', 'pricing_parity' ); ?>
 					<?php else : ?>
 					<?php echo $discount['country']; ?> - <?php echo $discount['amount']; ?>%
 					<?php endif; ?>
-				</p>
+				</span>
 			</div>
 		<?php
 	}
