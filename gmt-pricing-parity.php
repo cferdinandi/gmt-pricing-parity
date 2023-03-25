f0dbd5d465e166d5ff85fa2d5be6f3867854cbef
cferdinandi <chris@gomakethings.com>
2023-03-25 10:17:49 -0400

    Namespace protection

--- gmt-pricing-parity.php
+++ gmt-pricing-parity.php
@@ -5,11 +5,15 @@
  * Plugin URI: https://github.com/cferdinandi/gmt-pricing-parity/
  * GitHub Plugin URI: https://github.com/cferdinandi/gmt-pricing-parity/
  * Description: Provide custom discounts based on geographic location
- * Version: 2.8.0
+ * Version: 2.8.1
  * Author: Chris Ferdinandi
  * Author URI: http://gomakethings.com
  * License: GPLv3
  */
+
+// Security
+if (!defined('ABSPATH')) exit;
+if (!class_exists( 'EDD' )) exit;
 
 // Utilities
 require_once( plugin_dir_path( __FILE__ ) . 'includes/helpers.php' );
